353621d584414c83a15cebde8c3d5fa6dab880c7
Aleksandr Ataev <onlyaa94@mail.ru>
2023-08-04 01:01:26 +0300

    Initial Commit

---
+++ Memorize Test App/Memorize_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_Test_AppApp.swift
+//  Memorize Test App
+//
+//  Created by Aleksandr Ataev on 04.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
