e69fdfaeee937d7c5512454d93f97b6c204544b8
Ataev Aleksandr <onlyaa94@mail.ru>
2023-08-08 14:59:03 +0300

    Add model

---
+++ Memorize Test App/MemoryGame.swift
@@ -0,0 +1,20 @@
+//
+//  MemoryGame.swift
+//  Memorize Test App
+//
+//  Created by Ataev Aleksandr on 08.08.2023.
+//
+
+import Foundation
+
+struct MemoryGame {
+    var cards: Array<Card>
+    
+    func choose(_ card: Card) {
+        
+    }
+    
+    struct Card {
+        
+    }
+}
